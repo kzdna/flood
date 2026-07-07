a56e1d8536b102e1133763eb147b6e7e032a5698
Salsa Dharma Arindina <salsadina2005@gmail.com>
2026-07-07 19:05:18 +0700

    Fixed

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -8,6 +8,8 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;600;700&family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
+<p>asset: {{ asset('css/landing.css') }}</p>
+<p>secure_asset: {{ secure_asset('css/landing.css') }}</p>
 <link rel="stylesheet" href="https://fortunate-mindfulness-production-d9ef.up.railway.app/css/landing.css">
 </head>
 <body>
