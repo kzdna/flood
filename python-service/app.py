c37d839bf9effcad810512598d72038fb72ddd0b
Salsa Dharma Arindina <salsadina2005@gmail.com>
2026-07-07 21:35:01 +0700

    Coba

--- python-service/app.py
+++ python-service/app.py
@@ -99,7 +99,7 @@ model.eval()
 print("✅ Model berhasil dimuat")
 
 transform = transforms.Compose([
-    transforms.Resize((256, 256)),
+    transforms.Resize((128, 128)),
     transforms.ToTensor(),
     transforms.Normalize(
         mean=(0.485, 0.456, 0.406),
